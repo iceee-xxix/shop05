f843a6f7fda60fc1a945bf2cac1cb5b0ef0eb3d1
iceeee <kenzo.lay1936@gmail.com>
2025-04-17 15:28:43 +0700

    fix: update migration

--- database/migrations/2025_04_17_070238_create_categories_table.php
+++ database/migrations/2025_04_17_070238_create_categories_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('icon');
+            $table->text('image');
             $table->timestamps();
             $table->softDeletes();
         });
