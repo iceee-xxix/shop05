bea49f8bd5bc05aa8c0e78c0d11c86f5556773d1
iceeee <kenzo.lay1936@gmail.com>
2025-04-22 09:24:32 +0700

    fix: demo

--- resources/views/users/main_page.blade.php
+++ resources/views/users/main_page.blade.php
@@ -91,7 +91,7 @@ $config = Config::first();
 @endif
 <div class="container mt-1">
     <div class="d-flex flex-column justify-content-center">
-        <!-- <div class=" text-start fw-bold fs-5" style="color:<?= ($config->color_font != '')  ? $config->color_font :  '#ffffff' ?>">
+        <div class=" text-start fw-bold fs-5" style="color:<?= ($config->color_font != '')  ? $config->color_font :  '#ffffff' ?>">
             ที่นี่เรามี...
         </div>
         <div class="overflow-x-auto d-flex justify-content-between gap-2 py-2"
@@ -123,7 +123,7 @@ $config = Config::first();
                 </div>
                 <div class="mt-1 fw-bold" style="font-size: 13px;color:<?= ($config->color_font != '')  ? $config->color_font :  '#ffffff' ?>;">ห้องน้ำสะอาด</div>
             </div>
-        </div> -->
+        </div>
         <div class="title-food">
             หมวดอาหาร
         </div>
